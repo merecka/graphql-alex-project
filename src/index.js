3863bf4eeac123745495ef9236f124949195af24
Alex Merecka <merecka@gmail.com>
2020-03-24 15:33:48 -0500

    Combined Apollo server and Sequelize initialization into index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 require("dotenv").config();
+
+//Configure and Initialize Apollo Server
 const { ApolloServer, gql } = require("apollo-server");
 
 const typeDefs = gql`
@@ -22,3 +24,24 @@ const server = new ApolloServer({
 server.listen({ port: process.env.PORT }).then(() => {
   console.log(`🚀 Server ready at ${process.env.PORT}`);
 });
+
+// Connect Sequelize to Postgres database
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize("alexmerecka", "alexmerecka", "password", {
+  host: "localhost",
+  dialect: "postgres",
+  pool: {
+    max: 9,
+    min: 0,
+    idle: 10000
+  }
+});
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Success!");
+  })
+  .catch(err => {
+    console.log(err);
+  });
