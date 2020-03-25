66113072c383e59466eec069232c6ba975a9aa0c
Alex Merecka <merecka@gmail.com>
2020-03-25 13:21:36 -0500

    Changed config.js file to use DATABASE_URL env variable for connection to the db

--- config/config.js
+++ config/config.js
@@ -1,11 +1,6 @@
 require("dotenv").config();
 module.exports = {
   development: {
-    username: process.env.DATABASE_USERNAME,
-    password: process.env.DATABASE_PASSWORD,
-    database: "alexmerecka",
-    host: "127.0.0.1",
-    dialect: "postgres",
-    operatorsAliases: false
+    use_env_variable: "DEV_DATABASE_URL"
   }
 };
